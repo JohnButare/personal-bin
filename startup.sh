3afcccd6fccb2c9033851f14c2dba2c564360bca
jjbutare <john.butare.s@gmail.com>
2014-01-17 13:06:58 -0700

    changes

--- startup.sh
+++ startup.sh
@@ -3,11 +3,13 @@
 app -b $command explorer AutoHotKey WinSplit
 
 # Host specific applications
+local common="word EverNote DropBox PowerMixer PowerMixer f.lux SnagIt pu"
 case "${COMPUTERNAME,,}" in
-	oversoul) app -b $command word EverNote DropBox PowerMixer SnagIt iCloud PowerPanel pu;;
-	minime) app -b $command word EverNote DropBox PowerMixer SnagIt iCloud pu;;
-	jjbutare-mobl) app -b $command word EverNote DropBox PowerMixer SnagIt CruiseControlTray hp pu; intel $command -b;;
-	jjbutare-mobl7) app -b $command word EverNote DropBox PowerMixer SnagIt CruiseControlTray pu; intel $command -b;;
+	bean) f.lux;;
+	minime) app -b $command $common;;
+	oversoul) app -b $command $common;;
+	jjbutare-mobl) app -b $command $common hp; intel $command -b;;
+	jjbutare-mobl7) app -b $command $common; intel $command -b;;
 esac
 
 # Other

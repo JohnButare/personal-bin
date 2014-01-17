682e4c8d97f8edad7fdc75918cd3ed402e187fa3
jjbutare <john.butare.s@gmail.com>
2014-01-17 12:13:26 -0700

    script changes

--- startup.sh
+++ startup.sh
@@ -1,24 +1,19 @@
 
 # Common applications
-app -b $command AutoHotKey WinSplit
+app -b $command explorer AutoHotKey WinSplit
 
 # Host specific applications
 case "${COMPUTERNAME,,}" in
-	oversoul) app -b $command LastPass word EverNote DropBox PowerMixer SnagIt iCloud PowerPanel pu;;
+	oversoul) app -b $command word EverNote DropBox PowerMixer SnagIt iCloud PowerPanel pu;;
 	minime) app -b $command word EverNote DropBox PowerMixer SnagIt iCloud pu;;
-	jjbutare-mobl) app -b $command LastPass word EverNote DropBox PowerMixer SnagIt CruiseControlTray hp pu; intel $command -b;;
+	jjbutare-mobl) app -b $command word EverNote DropBox PowerMixer SnagIt CruiseControlTray hp pu; intel $command -b;;
 	jjbutare-mobl7) app -b $command word EverNote DropBox PowerMixer SnagIt CruiseControlTray pu; intel $command -b;;
 esac
 
 # Other
-if [[ "$command" == "startup" ]]; then
-	app -b startup explorer
-	
-elif [[ "$command" == "close" ]]; then
-	#app -q close Chrome Firefox LastPass NotepadPP OneNote Outlook ProcessExplorer SnagIt\
-	#	Sonos TrueCrypt WindowsMediaPlayer pu Xming
+if [[ "$command" == "close" ]]; then
+	app -b close chrome LastPass notepadpp ProcessExplorer SnagIt	Sonos TrueCrypt pu
 	IsElevated && tc SqlServer service stop all
-	
 fi
 
 return $?

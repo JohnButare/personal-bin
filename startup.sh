a5945ef61f8edc336e68723bbc669a85dfd63c1e
John Butare <john.butare@intel.com>
2014-03-17 09:35:54 -0600

    Added Mosaico application

--- startup.sh
+++ startup.sh
@@ -1,6 +1,12 @@
 
 # Common applications
-app -b $command explorer AutoHotKey WinSplit
+app -b $command explorer AutoHotKey
+
+if [[ "$COMPUTERNAME" == @(jjbutare-ivm1) ]]; then
+	app -b mosaico
+else
+	app -b winsplit
+fi
 
 # Host specific applications
 local common="word EverNote DropBox PowerMixer PowerMixer f.lux SnagIt pu"

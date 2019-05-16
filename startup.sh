83c2d8e93f49e289afa64c603ed776d07a826e33
John Butare <john.butare@intel.com>
2019-05-16 12:54:10 -0600

    startup.sh: make PuttyAgent and sshd startup common

--- startup.sh
+++ startup.sh
@@ -2,9 +2,9 @@ app -b $command AutoHotKey
 
 xserver $command || return
 
-local common="AquaSnap TidyTabs AltTabTerminatror DropBox word Greenshot GlassWire"
+local common="AquaSnap TidyTabs AltTabTerminatror DropBox word Greenshot GlassWire PuttyAgent sshd"
 case "$HOSTNAME" in
-	oversoul) app -b $command CorsairUtilityEngine $common IntelRapidStorage vmware PuttyAgent sshd;;
+	oversoul) app -b $command CorsairUtilityEngine $common IntelRapidStorage vmware;;
 	bean) f.lux;;
 	jjbutare-wvm*) app -b $command $common;;
 	jjbutare-i*|jjbutare-mobl*) app -b $command $common SyncPlicity; intel $command -b;;

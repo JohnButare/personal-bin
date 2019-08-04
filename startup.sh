a264c934ef40cd2f564f8a37c60d0db8d685bdda
John Butare <john.butare@intel.com>
2019-08-04 10:46:07 -0600

    startup.sh: add Discord

--- startup.sh
+++ startup.sh
@@ -1,10 +1,11 @@
-local common="AquaSnap TidyTabs AltTabTerminator DropBox LogitechOptions word Greenshot GlassWire PuttyAgent sshd"
+local file="DropBox OneDrive"
+local common="AquaSnap TidyTabs AltTabTerminator LogitechOptions word Greenshot GlassWire PuttyAgent sshd $file"
 
 app -b $command AutoHotKey || return
 xserver $command || return
 
 case "$HOSTNAME" in
-	oversoul) app -b $command $common CorsairUtilityEngine IntelRapidStorage || return;;
+	oversoul) app -b $command $common CorsairUtilityEngine IntelRapidStorage Discord || return;;
 	bean) f.lux || return;;
 	jjbutare-wvm*) app -b $command $common || return;;
 	jjbutare-i*|jjbutare-mobl*) app -b $command $common SyncPlicity || return; intel $command -b || return;;

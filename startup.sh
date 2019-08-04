a98c8e36214a51005fbf4def53e3959f9914aae1
John Butare <john.butare@intel.com>
2019-08-03 20:21:10 -0600

    startup.sh: fixes

--- startup.sh
+++ startup.sh
@@ -1,14 +1,13 @@
-local common="AquaSnap TidyTabs AltTabTerminatror DropBox word Greenshot GlassWire PuttyAgent sshd"
+local common="AquaSnap TidyTabs AltTabTerminator DropBox LogitechOptions word Greenshot GlassWire PuttyAgent sshd"
 
 app -b $command AutoHotKey || return
 xserver $command || return
 
 case "$HOSTNAME" in
-	oversoul) app -b $command $common CorsairUtilityEngine IntelRapidStorage;;
+	oversoul) app -b $command $common CorsairUtilityEngine IntelRapidStorage || return;;
 	bean) f.lux || return;;
 	jjbutare-wvm*) app -b $command $common || return;;
 	jjbutare-i*|jjbutare-mobl*) app -b $command $common SyncPlicity || return; intel $command -b || return;;
 esac
-
 
 return 0

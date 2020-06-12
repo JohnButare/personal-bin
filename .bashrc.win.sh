8620fcf744af29556f2df801767ee63b5bc6ce76
John Butare <john@butare.net>
2020-06-12 17:12:48 -0600

    .bashrc..bashrc.win.sh: add wsl testing aliases

--- .bashrc.win.sh
+++ .bashrc.win.sh
@@ -1,10 +1,10 @@
 
 # applications
+alias cmd="cmd.exe"
 alias autoruns='start --elevate autoruns.exe'
 alias powershell="$WINDIR/system32/WindowsPowerShell/v1.0/powershell.exe"
 alias wmic="$WINDIR/system32/wbem/WMIC.exe"
 rdesk() { ( mstsc.exe '/f' '/v:'"${@}" & ) }
-wsld="$DATAD/data/wsl"
 
 # monitor
 alias monden='monprofile den'
@@ -33,3 +33,13 @@ alias cs='start cscript.exe /nologo'
 # utilities
 alias ffw='elevate powershell FlipFlopWheel.ps1'
 z7bak() { [[ $# == 1  ]] && 7z a -m1=LZMA2 "$1.7z" "$1" || 7z a -m1=LZMA2 "$1" "${@:2}"; }
+
+# wsl
+wsld="$DATAD/data/wsl" # dir
+
+# test1 distribution
+alias wi="wsl init test1" # Initialize
+alias wr="wsl run test1" # Run
+alias wt="wslr test1 ubuntu-focal" # reseT
+
+wslr() { wsl --no-prompt delete "$1"; wsl restore "$1" "$2" 2; wsl init "$1"; } # reset DIST SRC

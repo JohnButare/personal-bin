04750d2f3d6e27fa8dfb80f6d7d0168f6536da8b
John Butare <john@butare.net>
2020-06-15 11:29:25 -0600

    .bashrc: update wsl aliases

--- .bashrc.win.sh
+++ .bashrc.win.sh
@@ -38,13 +38,13 @@ z7bak() { [[ $# == 1  ]] && 7z a -m1=LZMA2 "$1.7z" "$1" || 7z a -m1=LZMA2 "$1" "
 wsld="$DATAD/data/wsl" # dir
 
 # test1 distribution
-wi() { wsl init test1 "$@"; } 					# Initialize
-wt() { wslr test1 ubuntu-focal "$@"; } 	# reseT
-wr() { wsl run test1 "$@"; } 						# Run
+wreset1() { wslr test1 ubuntu-bionic 1 "$@"; } 	# reset as Ubuntu-Bionic (18.04) WSL 1
+wreset2() { wslr test1 ubuntu-focal 2 "$@"; } 	# reset as Ubuntu-Focal (20.04) WSL 2
+wi() { wsl init test1 "$@"; } 									# initialize test1 - run bootstrap-init
+wr() { wsl run test1 "$@"; } 										# run test1
 
-alias wsldown="wsl.exe --shutdown"
+wn() { echo "$(wsl name) (WSL $WSL)"; } 							# name of current distribution
+wv() { echo "WSL $(wsl version)" | figlet | lolcat; } # version of current distribution
 
-wn() { echo "$(wsl name) (WSL $WSL)"; }
-wv() { echo "WSL $(wsl version)" | figlet | lolcat; }
- 
-wslr() { wsl delete "$1" "$@"; wsl restore "$1" "$2" 2; wsl init "$1"; } # reset DIST SRC
+wsldown() { wsl.exe --shutdown; } 										# shutdown all distributions
+wslr() { wsl delete "$1" "$@"; wsl restore "$1" "$2" "${3:-2}"; wsl init "$1"; } # reset DIST SRC

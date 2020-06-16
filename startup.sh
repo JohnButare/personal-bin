92fe556cd57a5109b5555ab4090a7b5e90466ffb
John Butare <john@butare.net>
2020-06-16 15:42:35 -0600

    startup.sh: add PowerPanel

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 xserver $command || return
-app -b $command AutoHotKey AquaSnap CorsairUtilityEngine duet TidyTabs AltTabTerminator LogitechOptions word Greenshot PuttyAgent sshd DropBox terminator || return
+app -b $command AutoHotKey AquaSnap CorsairUtilityEngine duet TidyTabs AltTabTerminator LogitechOptions PowerPanel word Greenshot PuttyAgent sshd DropBox terminator || return
 return 0

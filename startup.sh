993b928aebf2eaa1e6fee39ffc9affa222dcace5
John Butare <john@butare.net>
2020-09-07 17:33:22 -0600

    startup.sh: do not autostart Docker (high memory)

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 xserver $command || return
 
 # service
-app -b $command chrony docker sshd  || return
+app -b $command chrony sshd  || return # docker
 
 # applications - dropbox shows false start initially, try again at end
 app -b $command dropbox AutoHotKey AquaSnap LogitechOptions greenshot PuttyAgent word dropbox || return

e2049e3f795e1ffbbfd2ac5fe1c21e4217016211
John Butare <john@butare.net>
2020-09-07 20:01:34 -0600

    startup.sh: remove word and PuttyAgent

--- startup.sh
+++ startup.sh
@@ -4,6 +4,6 @@ xserver $command || return
 app -b $command chrony sshd  || return # docker
 
 # applications - dropbox shows false start initially, try again at end
-app -b $command dropbox AutoHotKey AquaSnap LogitechOptions greenshot PuttyAgent word dropbox || return
+app -b $command dropbox AutoHotKey AquaSnap LogitechOptions greenshot dropbox || return
 
 return 0

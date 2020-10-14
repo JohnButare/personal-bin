99d7fd151b252ba403fddfef5bd901c44797e555
John Butare <john@butare.net>
2020-10-14 12:33:40 -0600

    startup.sh: add incron

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 xserver $command || return
 
 # service
-app -b $command dbus chrony cron sshd  || return # docker
+app -b $command dbus chrony cron incron sshd  || return # docker
 
 # applications - dropbox shows false start initially, try again at end
 app -b $command dropbox AutoHotKey AquaSnap LogitechOptions greenshot dropbox || return

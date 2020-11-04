451631f8b451d4678fc47a372a6bafbbcc24f77a
John Butare <john@butare.net>
2020-11-04 07:34:55 -0700

    startup.sh: update startup programs

--- startup.sh
+++ startup.sh
@@ -4,6 +4,6 @@
 # app -b $command sshd  || return # dbus docker chrony cron incron sshd
 
 # applications
-app -b $command AquaSnap || return # AquaSnap AutoHotKey LogitechOptions
+app -b $command AquaSnap AutoHotKey LogitechOptions || return # 
 
 return 0

7f563dc261ad4e2b4ab477279e4d0accd6bd263a
John Butare <johns@butare.net>
2021-02-22 13:06:12 -0700

    startup.sh: add pu

--- startup.sh
+++ startup.sh
@@ -4,6 +4,6 @@ xserver $command || return
 app -b $command dbus sshd  || return # dbus docker chrony cron incron sshd
 
 # applications
-app -b $command AquaSnap AutoHotKey LogitechOptions TidyTabs || return # TidyTabs
+app -b $command AquaSnap AutoHotKey LogitechOptions pu TidyTabs || return
 
 return 0

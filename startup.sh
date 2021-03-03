8495e246911cbb07dae8bec36a49676290997967
John Butare <johns@butare.net>
2021-03-03 15:05:46 -0700

    startup.sh: remove TidyTabs

--- startup.sh
+++ startup.sh
@@ -4,6 +4,6 @@ xserver $command || return
 app -b $command dbus sshd  || return # dbus docker chrony cron incron sshd
 
 # applications
-app -b $command AquaSnap AutoHotKey LogitechOptions pu TidyTabs || return
+app -b $command AquaSnap AutoHotKey LogitechOptions pu || return # TidyTabs
 
 return 0

00db67571403cad3e9c25c57926fa4863550abac
John Butare <johns@butare.net>
2021-03-15 14:08:52 -0600

    startup.sh: updates
    - add chrony for Hyper-V
    - add network

--- startup.sh
+++ startup.sh
@@ -1,7 +1,13 @@
 xserver $command || return
 
+# network
+app -b $command network || return
+
 # service
-app -b $command dbus sshd  || return # dbus docker chrony cron incron sshd
+app -b $command dbus sshd || return # dbus docker chrony cron incron sshd
+
+# fix time drift in WSL under Hyper-V
+IsPlatform wsl && IsHypervVm && { app -b $command chrony time || return; } 	
 
 # applications
 app -b $command AquaSnap AutoHotKey LogitechOptions pu || return # TidyTabs

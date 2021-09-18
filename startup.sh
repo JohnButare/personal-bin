afb8fbf4bee7a8735a2d3ac8127492ba8a70dd25
John Butare <johns@butare.net>
2021-09-17 21:48:30 -0600

    startup.sh: add docker

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 app -b $command xserver network || return
 
 # service
-app -b $command dbus sshd || return # dbus docker chrony cron incron sshd
+app -b $command dbus docker sshd || return # dbus docker chrony cron incron sshd
 
 # chrony - fixes time drift in WSL under Hyper-V
 IsPlatform wsl && IsHypervVm && { app -b $command chrony time || return; } 	

083f033bbbb0ecd34a5cc2d15ad142bed757cfda
John Butare <johns@butare.net>
2022-12-09 13:11:22 -0700

    startup.sh: add slack

--- startup.sh
+++ startup.sh
@@ -14,7 +14,7 @@ st docker || return
 IsPlatform wsl && IsHypervVm && { st chrony time || return; } 	
 
 # applications
-st WindowManager AutoHotKey LogitechOptions pu teams || return
+st WindowManager AutoHotKey LogitechOptions pu slack teams || return
 
 # specific applciations
 case "$HOSTNAME" in

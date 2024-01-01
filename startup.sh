aa75e88d90180a8a644ec7d5bb61cb74979e9cab
John Butare <johns@butare.net>
2024-01-01 12:51:10 -0700

    startup.sh: run time synchronization last

--- startup.sh
+++ startup.sh
@@ -10,8 +10,8 @@ st xserver dbus || return
 # start SSH before port forwarding (ports check uses SSH)
 st sshd ports || return
 
-# other services - chrony docker cron incron
-st chrony docker || return 
+# other services - docker cron incron
+st docker || return 
 
 # clock - fix time drift in WSL and major differences when Hyper-V guest resumes
 IsPlatform wsl && { st time || return; }
@@ -23,5 +23,8 @@ st AutoHotKey alttab pu UltraMon || return		# win
 st alfred alttab rectangle shottr || return		# mac
 
 IsPlatform parallels && { drive mount all --no-optical || return; }
+
+# time synchronization - run last, can fail
+st chrony || return 
 
 return 0

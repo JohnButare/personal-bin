bf6b986cbd64bc43e75aab8ce033592c06c429f4
John Butare <johns@butare.net>
2024-06-02 15:17:11 -0600

    startup.sh: domain fixes

--- startup.sh
+++ startup.sh
@@ -5,24 +5,28 @@ export CACHED_PROCESSES="$(ProcessList)"
 st network
 
 # start X Server and D-Bus (for credentials)
-st xserver dbus
+st xserver
+! { IsPlatform win && IsSystemd; } && st dbus # Unable to set up transient service directory
 
 # start SSH before port forwarding (ports check uses SSH)
-st sshd ports
+st sshd
+! IsInDomain && st ports
 
 # other services - docker cron incron nix
 st docker nix
 
 # clock - fix time drift in WSL and major differences when Hyper-V guest resumes
-IsPlatform wsl && { st time; }
+! IsInDomain && IsPlatform wsl && { st time; }
 
 # applications
 st WindowManager																		# start first
 st dropbox																					# shared
-st AutoHotKey pu UltraMon														# win
+st AutoHotKey UltraMon															# win
 st alfred alttab moom rectangle shottr || return		# mac
 
 IsPlatform parallels && { drive mount all --no-optical || return; }
 
 # time synchronization - run last, can fail
 st chrony
+
+return 0

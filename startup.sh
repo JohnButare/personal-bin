b097ed3c9da2b40c4cac559f2bdebe9fc044bafa
John Butare <johns@butare.net>
2025-01-11 13:29:12 -0700

    startup.sh: improvements
    - add BgInfo
    - docker only for bl?
    - remove returns

--- startup.sh
+++ startup.sh
@@ -4,25 +4,28 @@ export CACHED_PROCESSES="$(ProcessList)"
 # ensure network configure is correct first
 st network
 
+# desktop background
+[[ "$HOSTNAME" == @(bc|bl?) ]] && st BgInfo
+
+
 # start X Server and D-Bus (for credentials)
 st xserver
 ! { IsPlatform win && IsSystemd; } && st dbus # Unable to set up transient service directory
 
 # other services - docker cron incron nix ssh ports (ports check uses SSH)
-st docker nix sshd ports
+[[ "$HOSTNAME" == @(bl?) ]] && st docker
+st nix sshd ports
 
 # clock - fix time drift in WSL and major differences when Hyper-V guest resumes
 ! IsDomainRestricted && IsPlatform wsl && { st time; }
 
 # applications
-st WindowManager																		# start first
-st dropbox OneDrive																	# shared
-st AutoHotKey UltraMon															# win
-st alfred AltTab moom rectangle shottr || return		# mac
+st WindowManager													# start first
+st dropbox OneDrive												# shared
+st AutoHotKey UltraMon										# win
+st alfred AltTab moom rectangle shottr		# mac
 
 IsPlatform parallels && { drive mount all --no-optical || return; }
 
 # time synchronization - run last, can fail
 st chrony
-
-return 0

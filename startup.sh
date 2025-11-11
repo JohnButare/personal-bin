ec0b547858e30ae79f9fafb02da2e171b060c720
John Butare <johns@butare.net>
2025-11-11 15:29:15 -0700

    startup.sh: add globalArgs

--- startup.sh
+++ startup.sh
@@ -1,20 +1,24 @@
 # cache processes for faster checks below
 export CACHED_PROCESSES="$(ProcessList)"
 
+# arguments
+a=()
+
 # initial setup
 st network xserver WindowManager
-! IsDomainRestricted && IsPlatform wsl && st time # fix time drift in WSL and major differences when Hyper-V guest resumes
-! { IsPlatform win && IsSystemd; } && st dbus
+! IsDomainRestricted && IsPlatform wsl && st time "${a[@]}" # fix time drift in WSL and major differences when Hyper-V guest resumes
+! { IsPlatform win && IsSystemd; } && st dbus "${a[@]}"
 
 # applications
-st 1Password dropbox OneDrive AnyplaceUSB						# shared
-st AutoHotKey UltraMon															# win
-st alfred AltTab bartender moom rectangle shottr		# mac
+st 1Password dropbox OneDrive AnyplaceUSB	"${a[@]}"					# shared
+st AutoHotKey UltraMon "${a[@]}"													 	# win
+st alfred AltTab bartender moom rectangle shottr "${a[@]}" 	# mac
 IsPlatform parallels && { drive mount all --no-optical || return; }
-[[ "$HOSTNAME" == @(bl?) ]] && st BgInfo
+[[ "$HOSTNAME" == @(bl?) ]] && st BgInfo "${a[@]}"
 
 # services
-st chrony nix sshd
-[[ "$HOSTNAME" == @(bl?) ]] && st docker consul nomad guacamole
+a=(-vvvvv)
+st chrony nix sshd "${a[@]}"
+[[ "$HOSTNAME" == @(bl?) ]] && st docker consul nomad guacamole "${a[@]}"
 
 return 0
